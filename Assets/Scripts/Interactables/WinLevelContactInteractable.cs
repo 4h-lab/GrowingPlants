52f1ffbf1e902bff8500201dd8d8c612543302c6
fedebamba <federicobambagioni@gmail.com>
2020-03-12 10:42:45 -0700

    updated interactions

--- Assets/Scripts/Interactables/WinLevelContactInteractable.cs
+++ Assets/Scripts/Interactables/WinLevelContactInteractable.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinLevelContactInteractable : BaseContactInteractables{
     public override void interact(GameObject initiator){
         Debug.Log("You won!!!!!!");
+        SceneManager.LoadSceneAsync("prereleaseWinScene");
     }
 }
