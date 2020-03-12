a9ce224758a4ce4c61be45500e5b7ffcdd0cd91a
fedebamba <federicobambagioni@gmail.com>
2020-03-12 11:29:51 -0700

    fede

---
+++ Assets/Scripts/Interactables/ContactInteractables/DamageContactInteractable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageContactInteractable : BaseContactInteractables{
+    public int damage;
+    public override void interact(GameObject initiator){
+        Health health = initiator.GetComponent<Health>();
+        if (health != null) {
+            health.damage(damage);
+        }
+
+    }
+}
