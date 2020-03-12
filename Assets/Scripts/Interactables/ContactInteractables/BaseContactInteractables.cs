5e72a6c84b1f53bf3bba130a033ff0a2f061038e
fedebamba <federicobambagioni@gmail.com>
2020-03-12 10:31:50 -0700

    interactables

---
+++ Assets/Scripts/Interactables/ContactInteractables/BaseContactInteractables.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseContactInteractables : BaseInteractable{
+
+
+    private void OnTriggerEnter(Collider other){
+        GameObject go = other.gameObject;
+        interact(go);
+    }
+
+
+}
---
+++ Assets/Scripts/Interactables/WinLevelContactInteractable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinLevelContactInteractable : BaseContactInteractables{
+    public override void interact(GameObject initiator){
+        Debug.Log("You won!!!!!!");
+    }
+}
