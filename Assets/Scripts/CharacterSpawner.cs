410e08e49136582da34fd3003af70ba3ea294ed3
fedebamba <federicobambagioni@gmail.com>
2020-03-12 08:39:18 -0700

    scroto

---
+++ Assets/Scripts/CharacterSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSpawner : MonoBehaviour{
+    public void requireSpawn() {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up); // fire a raycast directly down the player
+
+        if (hit.collider != null) { 
+            SpawnerTile st = hit.collider.gameObject.GetComponent<SpawnerTile>();
+            if (st != null) { // check whether the object hit has a spawnertile component (that means, if it can spawn plants)
+                st.spawn(gameObject); // invoke spawn passing the player as arg
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Spawner/SpawnerTile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerTile : MonoBehaviour{
+    public GameObject spawnableObject;
+
+    public void spawn(GameObject initiator) {
+        GameObject.Instantiate(spawnableObject, initiator.transform);
+    }
+
+}
