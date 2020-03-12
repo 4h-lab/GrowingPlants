4be90475e2f50edea2aa83fc3627a32d64e0e6a2
fedebamba <federicobambagioni@gmail.com>
2020-03-12 09:59:04 -0700

    Completed plant framework
    - plants can now grow until they reach the specified height

--- Assets/Scripts/Spawner/Plants/NormalPlant.cs
+++ Assets/Scripts/Spawner/Plants/NormalPlant.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class NormalPlant : MonoBehaviour{
     public float maxHeigth;
     public float growthSpeed;
-    
+
+    private float initY;
+
     // Start is called before the first frame update
     void Start(){
-        
+        initY = this.transform.position.y;
     }
 
     // Update is called once per frame
     void Update(){
-        transform.Translate(Vector2.up * Time.deltaTime * growthSpeed );
+        if (transform.position.y < initY + maxHeigth){
+            transform.Translate(Vector2.up * Time.deltaTime * growthSpeed);
+        }
+
     }
+        
 }
