100d7813e638220433f5446ccfdf0f1b5682ce0f
Gianmarco Biscini <g.biscini@gmail.com>
2020-03-12 17:45:07 +0100

    movement

--- Assets/Scripts/Movement/Movement.cs
+++ Assets/Scripts/Movement/Movement.cs
@@ -7,10 +7,13 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField]
     private float speed = 1.0f;
+    private bool moveRight=false;
+    private bool moveLeft=false;
+    private Rigidbody2D rigidbody2D;
     // Start is called before the first frame update
     void Start()
     {
-
+        rigidbody2D = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,18 +23,39 @@ public class Movement : MonoBehaviour
             transform.position -= Vector3.right * speed * Time.deltaTime;
         if (Input.GetKey(KeyCode.D))
             transform.position += Vector3.right * speed * Time.deltaTime;
+
+        if (moveLeft && !moveRight)
+            rigidbody2D.AddForce(Vector3.left * speed);
+
+        if (moveRight && !moveLeft)
+            rigidbody2D.AddForce(Vector2.right * speed);
     }
 
     public void move(Vector2 dir)
     {
         this.transform.position = dir * speed * Time.deltaTime;
     }
-    public void moveRight()
+    public void startRight()
     {
-        this.transform.position += Vector3.right * speed * Time.deltaTime;
+        //Debug.Log("move");
+        //this.transform.position += Vector3.right * speed * Time.deltaTime;
+        moveRight = true;
     }
-    public void moveLeft()
+
+    public void stopRight()
     {
-        this.transform.position += -Vector3.right * speed * Time.deltaTime;
+        //Debug.Log("move");
+        //this.transform.position += Vector3.right * speed * Time.deltaTime;
+        moveRight = false;
+    }
+    public void startLeft()
+    {
+        //this.transform.position += -Vector3.right * speed * Time.deltaTime;
+        moveLeft = true;
+    }
+    public void stopLeft()
+    {
+        //this.transform.position += -Vector3.right * speed * Time.deltaTime;
+        moveLeft = false;
     }
 }
