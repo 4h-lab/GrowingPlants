b921ab7f84b6f9eccc406375bd1cb12999b64c94
fedebamba <federicobambagioni@gmail.com>
2020-03-13 04:20:38 -0700

    dadada dududu

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour{
+    public int maxHealth;
+    public int health { get; private set; } 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void damage(int amount) {
+        health -= amount;
+        if (health > maxHealth) health = maxHealth;
+        if (health <= 0) {
+            die();
+        }
+    }
+    
+    private void die() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
---
+++ Assets/Scripts/Interactables/ContactInteractables/DamageContactInteractable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageContactInteractable : BaseContactInteractables{
+    public int damage;
+
+    public override void interact(GameObject initiator) {
+        Health h = initiator.GetComponent<Health>();
+        if (h != null) h.damage(damage);
+    }
+}
