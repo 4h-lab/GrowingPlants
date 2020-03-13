7a44bdc22d0d9b78e1326e124c9b46b7199fe542
Gianmarco Biscini <g.biscini@gmail.com>
2020-03-13 12:37:19 +0100

    LAA

--- Assets/Scripts/Movement/MovementJoystick.cs
+++ Assets/Scripts/Movement/MovementJoystick.cs
@@ -11,5 +11,10 @@ public class MovementJoystick : MonoBehaviour
     {
         Vector3 direction =  Vector3.right * variableJoystick.Horizontal;
         transform.position += direction * speed * Time.deltaTime;
+        /////////DEBUG
+        if (Input.GetKey(KeyCode.A))
+            transform.position -= Vector3.right * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D))
+            transform.position += Vector3.right * speed * Time.deltaTime;
     }
 }
