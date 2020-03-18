d131b7331451bcaeeab7ce529aea20844d0a216b
fedebamba <federicobambagioni@gmail.com>
2020-03-18 11:15:20 -0700

    j

--- Assets/Scripts/PlantButton.cs
+++ Assets/Scripts/PlantButton.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlantButton : MonoBehaviour
-{
-    void FixedUpdate()
+public class PlantButton : MonoBehaviour{
+    //GameObject player
+    void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
             //TODO: change to simulate click if needed
             //https://answers.unity.com/questions/945299/how-to-trigger-a-button-click-from-script.html
             gameObject.GetComponent<Button>().onClick.Invoke();
+            //gameObject.GetComponent<>
         }
     }
 }
