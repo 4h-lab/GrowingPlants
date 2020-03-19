8333beef50b504bdfb93fd17938afb54571ad686
fedebamba <federicobambagioni@gmail.com>
2020-03-19 12:57:21 -0700

    added pause button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-    public static float customTimeScale { get; private set; } = 1f;
+    public static float customTimeScale { get;  private set; } = 1f;
 
     EventEmitter ee;
 
@@ -12,5 +12,13 @@ public class GameManager : MonoBehaviour {
         ee.on("win", (Object[] x) => { customTimeScale = 0f; });
         customTimeScale = 1f;
     }
+
+    public void setPause(bool pause){
+        customTimeScale = pause ? 0f : 1f;
+    }
+    public void setPause() {
+        customTimeScale = customTimeScale != 0f ? 0f :1f;
+    }
+
 
 }
