eece876798a7b52504e0480059e0bff1009d909d
Gianmarco Biscini <g.biscini@gmail.com>
2020-03-19 16:45:34 +0100

    added plant that stops on objects (not working yet)

---
+++ Assets/Scripts/Interactables/ContactInteractables/PlantContactInteractable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantContactInteractable : BaseContactInteractables
+{
+    private EventEmitter ee;
+
+    private void Start()
+    {
+        ee = GameObject.FindGameObjectWithTag("EventEmitter").GetComponent<EventEmitter>();
+    }
+    public override void interact(GameObject initiator)
+    {
+        if (initiator.GetComponent<CharacterSpawner>()!=null)
+        {
+            return;
+        }
+        this.transform.parent.GetComponent<NormalPlant>().hitStuff();
+    }
+}
