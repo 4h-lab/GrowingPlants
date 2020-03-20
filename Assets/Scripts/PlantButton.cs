961bf87760de5abfa3d22101a33cff31074c618d
Xe913 <simonegrossi@gmail.com>
2020-03-20 10:38:04 +0100

    Fix: can't spawn plants with spacebar during Pause anymore

--- Assets/Scripts/PlantButton.cs
+++ Assets/Scripts/PlantButton.cs
@@ -7,7 +7,7 @@ public class PlantButton : MonoBehaviour{
     //GameObject player
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && FindObjectOfType<GameManager>().GetComponent<GameManager>().GetCustomTimeScale() != 0f)
+        if (Input.GetKeyDown(KeyCode.Space) && gameObject.GetComponent<Button>().enabled)
         {
             //TODO: change to simulate click if needed
             //https://answers.unity.com/questions/945299/how-to-trigger-a-button-click-from-script.html
