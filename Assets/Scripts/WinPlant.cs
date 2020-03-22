ab51f40ab82e965a61c47181b52283fce77b9ed4
Xe913 <simonegrossi@gmail.com>
2020-03-22 21:16:37 +0100

    Add WinSoil and WinPlant (it's a UFO right now)

---
+++ Assets/Scripts/WinPlant.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinPlant : MonoBehaviour
+{
+    public float maxHeigth;
+    public float growthSpeed;
+
+    private float initY;
+
+    private EventEmitter ee;
+    private GameObject player;
+    private bool stopped = false;
+    private float ray_point;
+    private float small_radius = 0.01f;
+
+    void Start()
+    {
+        ee = GameObject.FindGameObjectWithTag("EventEmitter").GetComponent<EventEmitter>();
+        ee.invoke("plant_created", (new[] { this.gameObject }));
+        player = GameObject.Find("Player");
+
+        //TODO: block camera
+        Destroy(player.GetComponent<Rigidbody2D>());
+        Destroy(player.GetComponent<BoxCollider2D>());
+        Destroy(GameObject.Find("SquishCollider"));
+    }
+
+    void Update()
+    {
+        if (GetComponent<SpriteRenderer>().isVisible) FlyAway();
+    }
+
+    private void OnBecameInvisible()
+    {
+        ee.invoke("win", (new[] { this.gameObject }));
+    }
+
+    private void FlyAway()
+    {
+        player.transform.Translate(Vector2.up * Time.deltaTime * growthSpeed * GameManager.customTimeScale);
+        gameObject.transform.Translate(Vector2.up * Time.deltaTime * growthSpeed * GameManager.customTimeScale);
+    }
+}
---
+++ Assets/Scripts/WinSoil.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+
+public class WinSoil : MonoBehaviour
+{
+    private EventEmitter ee;
+    [SerializeField] GameObject winScreen;
+
+    private int currentLevel = 1;
+    private int stars = 3;
+    public float time { get; private set; } = 0.0f;  //TODO: change to time format
+
+    void Start()
+    {
+        ee = GameObject.FindGameObjectWithTag("EventEmitter").GetComponent<EventEmitter>();
+        ee.on("win", notifyWin);
+        time = Time.realtimeSinceStartup;
+    }
+
+    private void notifyWin(Object[] p)
+    {
+        //placeholder
+        int currentLevel = 1;
+
+        FindObjectOfType<GameManager>().GetComponent<GameManager>().ControlsEnabled(false);
+        Debug.Log("WIN. Level " + currentLevel + " - stars " + stars + " - time " + time);
+        GameObject popup = GameObject.Instantiate(
+            winScreen,
+            Vector3.zero,
+            Quaternion.identity,
+            GameObject.FindObjectOfType<Canvas>().transform);
+        string obtainedStars = "";
+        for (int i = 0; i < stars; i++) obtainedStars += " *";
+        popup.transform.Find("StarsTextR").gameObject.GetComponent<TextMeshProUGUI>().text = obtainedStars;
+
+        time = Time.realtimeSinceStartup - time;
+        popup.transform.Find("TimeTextR").gameObject.GetComponent<TextMeshProUGUI>().text = formatGameTime(time);
+        popup.transform.Find("PauseMenuButtons").gameObject.GetComponent<PauseMenuButtons>().SetCurrentLevel(currentLevel);
+
+    }
+
+    private static string formatGameTime(float seconds)
+    {
+        Debug.Log(seconds);
+        int minutes = Mathf.FloorToInt(seconds / 60);
+        int s = Mathf.FloorToInt(seconds % 60);
+        int decimals = Mathf.FloorToInt((seconds * 100) % 100);
+
+        string res = (minutes < 10 ? "0" + minutes.ToString() : minutes.ToString());
+        res += ": " + (s < 10 ? "0" + s.ToString() : s.ToString());
+        res += ": " + (decimals < 10 ? "0" + decimals.ToString() : decimals.ToString());
+        return res;
+    }
+}
