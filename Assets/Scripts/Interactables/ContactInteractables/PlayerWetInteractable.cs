c1cdc1866e2d1a9b889187e5361e8b1a3bcd982e
fedebamba <federicobambagioni@gmail.com>
2020-03-23 10:14:34 -0700

    fixrd water

---
+++ Assets/Scripts/Interactables/ContactInteractables/PlayerWetInteractable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWetInteractable : BaseContactInteractables
+{
+    public override void interact(GameObject initiator)
+    {
+        if (initiator.GetComponent<PhysicWater>() != null) GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().notifyOfNewSomething("water.touchedby");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
