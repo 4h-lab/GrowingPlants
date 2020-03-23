d24e84868c0fabbcffb97aa1180d872e4b5bc98c
Gianmarco Biscini <g.biscini@gmail.com>
2020-03-23 15:29:53 +0100

    added falling platform

---
+++ Assets/Scripts/Interactables/CollisionInteractable/BaseCollisionInteractable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseCollisionInteractable : BaseInteractable
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject go = collision.gameObject;
+        interact(go);
+    }
+}
---
+++ Assets/Scripts/Interactables/ContactInteractables/FallingPlatformContactInteractable.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatformContactInteractable : BaseCollisionInteractable
+{
+    // Start is called before the first frame update
+    public float fallDelay = 2.0f;
+
+    private bool shaking=false;
+    private Vector3 originalPosition;
+
+    private void Start()
+    {
+        originalPosition = this.transform.position;
+    }
+    private void FixedUpdate()
+    {
+        if (shaking) this.transform.position =originalPosition+ (Random.insideUnitSphere*Time.deltaTime);
+
+    }
+
+    public override void interact(GameObject initiator)
+    {
+        if (initiator.tag == "Player")
+        {
+            shaking=true;
+            StartCoroutine(FallAfterDelay());
+        }
+    }
+    IEnumerator FallAfterDelay()
+    {
+        yield return new WaitForSeconds(fallDelay);
+        shaking = false;
+        this.gameObject.AddComponent<Rigidbody2D>();
+    }
+}
