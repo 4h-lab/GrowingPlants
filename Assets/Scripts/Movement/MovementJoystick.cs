4e82d0bfda1dca9a7f1d64a30a69de0cc84cb7a0
fedebamba <federicobambagioni@gmail.com>
2020-03-25 09:42:11 -0700

    modified player movement

--- Assets/Scripts/Movement/MovementJoystick.cs
+++ Assets/Scripts/Movement/MovementJoystick.cs
@@ -7,15 +7,29 @@ public class MovementJoystick : MonoBehaviour
 {
     private EventEmitter ee;
 
-    public float speed;
+    public float maxSpeed;
+    public float acceleration;
+    private float speed;
+
     public FixedJoystick variableJoystick;
 
     void Start() {
         ee = GameObject.FindGameObjectWithTag("EventEmitter").GetComponent<EventEmitter>();
+        speed = 0f;
     }
 
     public void FixedUpdate(){
+        Debug.Log(variableJoystick.Horizontal);
+        
+        if (variableJoystick.Horizontal != 0f) {
+            Vector3 dir = Vector3.right * Mathf.Sign(variableJoystick.Horizontal);
+            movePlayer(dir);
+        } 
+        else if (Input.GetKeyDown(KeyCode.A)) movePlayer(Vector3.left);
+        else if (Input.GetKeyDown(KeyCode.D)) movePlayer(Vector3.right);
+        else speed = 0;
 
+        /*
             Vector3 direction = Vector3.right * variableJoystick.Horizontal;
             transform.position += direction * speed * Time.deltaTime  *GameManager.customTimeScale;
             /////////DEBUG
@@ -23,6 +37,14 @@ public class MovementJoystick : MonoBehaviour
                 transform.position -= Vector3.right * speed * Time.deltaTime * GameManager.customTimeScale;
             if (Input.GetKey(KeyCode.D))
                transform.position += Vector3.right * speed * Time.deltaTime * GameManager.customTimeScale;
-        
+        */
     }
+    private void movePlayer(Vector3 dir) {
+        speed += acceleration * Time.deltaTime;
+        if (speed > maxSpeed) speed = maxSpeed;
+
+        transform.position += dir * speed * Time.deltaTime * GameManager.customTimeScale;
+    
+    }
+
 }
