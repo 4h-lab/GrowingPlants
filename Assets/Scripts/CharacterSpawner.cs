3a26174a9b44b6a0fafc46e9c81ccf1025923ebe
Gianmarco Biscini <g.biscini@gmail.com>
2020-03-26 17:08:36 +0100

    probable fix for spawner plants

--- Assets/Scripts/CharacterSpawner.cs
+++ Assets/Scripts/CharacterSpawner.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class CharacterSpawner : MonoBehaviour{
     private float radius;
+    private float radius_x;
 
     private void Start()
     {
         radius= this.gameObject.GetComponent<SpriteRenderer>().sprite.bounds.extents.y*this.transform.localScale.y;
-        
+        radius_x= this.gameObject.GetComponent<SpriteRenderer>().sprite.bounds.extents.x * this.transform.localScale.x;
+
     }
     public void requireSpawn() {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up,radius); // fire a raycast directly down the player
 
-     
+        //RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up,radius); // fire a raycast directly down the player
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position, radius_x, -Vector2.up,radius-radius_x);
+
+
 
         if (hit.collider != null) {
             SpawnerTile st = hit.collider.gameObject.GetComponent<SpawnerTile>();
