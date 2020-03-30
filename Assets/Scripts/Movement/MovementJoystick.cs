926cfc38f5bc052e38a11a6a1894da4863c42779
Gianmarco Biscini <g.biscini@gmail.com>
2020-03-30 12:14:07 +0200

    modified movement joystick so that it changes the velocity and not the position. This way we can control many more thinkgs, for example flipping the character based on its direction

--- Assets/Scripts/Movement/MovementJoystick.cs
+++ Assets/Scripts/Movement/MovementJoystick.cs
@@ -10,6 +10,7 @@ public class MovementJoystick : MonoBehaviour
     public float maxSpeed;
     public float acceleration;
     private float speed;
+    bool facingRight = true;
 
     public FixedJoystick variableJoystick;
 
@@ -21,11 +22,14 @@ public class MovementJoystick : MonoBehaviour
     public void FixedUpdate(){
         if (variableJoystick.Horizontal != 0f) {
             Vector3 dir = Vector3.right * Mathf.Sign(variableJoystick.Horizontal);
-            movePlayer(dir);
-        } 
-        else if (Input.GetKey(KeyCode.A)) movePlayer(Vector3.left);
-        else if (Input.GetKey(KeyCode.D)) movePlayer(Vector3.right);
-        else speed = 0;
+            changeVelocity(variableJoystick.Horizontal);
+        }
+        else if (Input.GetKey(KeyCode.A)) changeVelocity(-1f);
+        else if (Input.GetKey(KeyCode.D)) changeVelocity(1f);
+        else {
+            speed = 0;
+            changeVelocity(0f);
+        }
     }
     private void movePlayer(Vector3 dir) {
         speed += acceleration * Time.deltaTime;
@@ -33,6 +37,29 @@ public class MovementJoystick : MonoBehaviour
 
         transform.position += dir * speed * Time.deltaTime * GameManager.customTimeScale;
     
+    }
+    private void changeVelocity(float f)
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(f * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        if (f > 0 && !facingRight)
+            // ... flip the player.
+            Flip();
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (f < 0 && facingRight)
+            // ... flip the player.
+            Flip();
+    }
+
+    void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = this.transform.localScale;
+        theScale.x *= -1;
+        this.transform.localScale = theScale;
     }
 
 }
