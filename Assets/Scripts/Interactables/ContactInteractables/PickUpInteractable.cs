6b70595be3aa1ceeda583e478fff5354078dc9ad
fedebamba <federicobambagioni@gmail.com>
2020-04-04 08:22:26 -0700

    Update PickUpInteractable.cs

--- Assets/Scripts/Interactables/ContactInteractables/PickUpInteractable.cs
+++ Assets/Scripts/Interactables/ContactInteractables/PickUpInteractable.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class PickUpInteractable : BaseContactInteractables{
     public override void interact(GameObject initiator){
+        if (!(initiator.tag == "Player")) return;
+        
         GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().notifyOfNewSomething("star.pickup");
         Destroy(this.gameObject);
     }
