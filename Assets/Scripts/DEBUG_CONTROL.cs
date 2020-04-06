00f3ebbc32918a73c28096c2d4c6b943102d98c8
fedebamba <federicobambagioni@gmail.com>
2020-04-06 07:22:40 -0700

    added debug controls

---
+++ Assets/Scripts/DEBUG_CONTROL.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DEBUG_CONTROL : MonoBehaviour{
+    
+    
+    void Start(){
+        
+        
+    }
+    
+    
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Tab))SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if(Input.GetKeyDown(KeyCode.LeftControl))SceneManager.LoadScene(Mathf.Max(0, SceneManager.GetActiveScene().buildIndex + 1));
+
+    }
+}
