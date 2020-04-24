0e09b2817b6d12ce44d68520b516c074d224c018
Gianmarco Biscini <g.biscini@gmail.com>
2020-04-24 16:26:12 +0200

    added select level menu work in progress

---
+++ Assets/Scripts/MenuStuff/LoadLevels.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.Events;
+
+public class LoadLevels : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject button;
+    private Dictionary<int, SavedGameData.LevelData> levels;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        levels = SavedGameData.gamedata.getLevelInfos();
+        UnityAction startView;
+        foreach (KeyValuePair<int, SavedGameData.LevelData> level in levels)
+        {
+            var c = Instantiate(button, this.transform);
+            c.transform.GetComponentInChildren<TextMeshProUGUI>().text = level.Value.levelID.ToString();
+            startView = () => loadLevel(level.Value.levelID);
+            if (level.Value.unlocked) { 
+            c.GetComponentInChildren<Button>().onClick.AddListener(startView);
+            }
+            else
+            {
+                c.GetComponentInChildren<Button>().interactable = false;
+            }
+            for (int i=0;i<level.Value.stars; i++)
+            {
+                c.transform.Find("Stars").transform.GetChild(i).GetComponent<Image>().color = new Color(1f, 1f, 1f, 1f);
+            }
+
+        }
+    }
+
+    private void loadLevel(int l)
+    {
+        SceneManager.LoadScene(l);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
