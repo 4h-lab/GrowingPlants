0d52f085973c6172099cd1e38c60e1fc989d6f45
Luca Ballarati <1uca8allarati@gmail.com>
2020-05-01 17:51:28 +0200

    added a check to make sure the player is squished only if it is standing on something

--- Assets/Scripts/Interactables/ContactInteractables/SquishContactInteractable.cs
+++ Assets/Scripts/Interactables/ContactInteractables/SquishContactInteractable.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class SquishContactInteractable : BaseContactInteractables
 {
-    public int damage;
-    private EventEmitter ee;
+    [SerializeField] private int damage;
+
+    [SerializeField] private GameObject player;
 
     private int passableObjectsLayerMask = 0;
+    private IsGrounded Gr;
+    private EventEmitter ee;
 
     void Start(){
         ee = GameObject.FindGameObjectWithTag("EventEmitter").GetComponent<EventEmitter>();
@@ -15,10 +18,13 @@ public class SquishContactInteractable : BaseContactInteractables
         ee.on("win", DeactivateSquishCollision);
 
         passableObjectsLayerMask = (1 << LayerMask.NameToLayer("onewayplatform")) | (1 << LayerMask.NameToLayer("plant")) | (1 << LayerMask.NameToLayer("collectible")) |(1<<2);
+
+        player = GameObject.Find("Player");
+        Gr = player.GetComponent<IsGrounded>();
     }
 
     public override void interact(GameObject initiator){
-        if (((1 << initiator.layer) & passableObjectsLayerMask) != 0) return;
+        if ((((1 << initiator.layer) & passableObjectsLayerMask) != 0) || !Gr.GetGrounded()) return;
 
         Health h = this.transform.parent.GetComponent<Health>();
         if (h != null) h.damage(damage);
