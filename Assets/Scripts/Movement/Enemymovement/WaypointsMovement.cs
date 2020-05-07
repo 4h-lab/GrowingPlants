27f44730782fe5d6faa362629a11fc416b66a40c
Xe913 <simonegrossi@gmail.com>
2020-05-07 18:40:36 +0200

    Add bounce against switchable walls. First draft

--- Assets/Scripts/Movement/Enemymovement/WaypointsMovement.cs
+++ Assets/Scripts/Movement/Enemymovement/WaypointsMovement.cs
@@ -6,15 +6,20 @@ public class WaypointsMovement : MonoBehaviour{
     public List<GameObject> wayPoints;
     public float speed;
     public float minDesiredDistanceToWaypoint = .01f;
+    [SerializeField] ContactFilter2D collisionFilter;
 
     private int wayPointIndex = 0;
     private Vector2 nextWayPoint;
+    private int direction = 1;
 
+    private Rigidbody2D rb;
     private List<Vector2> realWayPoints;
+    bool stop = false;
 
 
     // Start is called before the first frame update
     void Start(){
+        rb = GetComponent<Rigidbody2D>();
         wayPoints.Add(gameObject);
         realWayPoints = new List<Vector2>(wayPoints.Count);
 
@@ -27,10 +32,37 @@ public class WaypointsMovement : MonoBehaviour{
     // Update is called once per frame
     void Update(){
         float desiredDistance = Mathf.Min((nextWayPoint - (Vector2)transform.position).magnitude, speed * Time.deltaTime * GameManager.customTimeScale);
-        transform.Translate((nextWayPoint - (Vector2)transform.position).normalized * desiredDistance) ;
+        Vector2 nextPosition = (nextWayPoint - (Vector2)transform.position).normalized;
+        RaycastHit2D[] castResults = new RaycastHit2D[16];
+
+        int collisionCount = rb.Cast(nextPosition, collisionFilter, castResults, desiredDistance);
+        Debug.Log("count " + collisionCount);
+        if (collisionCount > 0)
+        {
+            Debug.Log("COLLISION");
+            stop = true;
+            ChangeDirection();
+            
+            nextPosition = (nextWayPoint - (Vector2)transform.position).normalized;
+        }
+
+        transform.Translate(nextPosition * desiredDistance);
         if (Vector2.Distance(transform.position, nextWayPoint) < minDesiredDistanceToWaypoint) {
-            wayPointIndex = (wayPointIndex + 1) % wayPoints.Count;
-            nextWayPoint = realWayPoints[wayPointIndex]; 
+            ChangeWaypoint();
         } 
+    }
+
+    int ChangeDirection()
+    {
+        direction = -direction;
+        ChangeWaypoint();
+        return direction;
+    }
+
+    int ChangeWaypoint()
+    {
+        wayPointIndex = (wayPoints.Count + wayPointIndex + direction) % wayPoints.Count;
+        nextWayPoint = realWayPoints[wayPointIndex];
+        return wayPointIndex;
     }
 }
