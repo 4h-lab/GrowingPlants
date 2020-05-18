9e2d2b6d5613c094103d708135900897a0b80e03
Xe913 <simonegrossi@gmail.com>
2020-05-18 21:34:24 +0200

    Add fading effect to TurnOffBlock

--- Assets/Scripts/Switch/TurnOffBlock.cs
+++ Assets/Scripts/Switch/TurnOffBlock.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class TurnOffBlock : BaseSwitchable
 {
     //TODO: remove when we have sprites
-    [SerializeField] private float deactivatedAlphaPlaceholder = 0f;
+    [Header("Placeholder")]
+    [SerializeField] private float deactivatedAlpha = 0f;
+    [SerializeField] private float fadingTime = 1f;
+    private float currentAlpha = 1;
+
+    //TEMP FUNCTION TO BE REMOVED WHEN SPRITES ARE IMPLEMENTED
+    void Update()
+    {
+        if (currentState == 0 && currentAlpha > deactivatedAlpha) ChangeColorPlaceholder();
+    }
 
     public override void StartSwitchable()
     {
-
+        //TEMP FUNCTION TO BE REMOVED WHEN SPRITES ARE IMPLEMENTED
+        currentAlpha = gameObject.GetComponent<SpriteRenderer>().color.a;
     }
 
     public override int ChangeState(BaseSwitcher switcher, string action)
@@ -28,7 +38,6 @@ public class TurnOffBlock : BaseSwitchable
         GetComponent<SwitchableSoil>()?.DeactivateSoil();
         gameObject.GetComponent<BoxCollider2D>().enabled = false;
         Destroy(gameObject.GetComponent<Rigidbody2D>());
-        ChangeColorPlaceholder(); //TEMP FUNCTION TO BE REMOVED WHEN SPRITES ARE IMPLEMENTED
         currentState = 0;
     }
 
@@ -36,7 +45,7 @@ public class TurnOffBlock : BaseSwitchable
     private void ChangeColorPlaceholder()
     {
         Color tmpColor = gameObject.GetComponent<SpriteRenderer>().color;
-        tmpColor.a = deactivatedAlphaPlaceholder;
+        tmpColor.a = tmpColor.a - (tmpColor.a - deactivatedAlpha)/fadingTime * Time.deltaTime;
         gameObject.GetComponent<SpriteRenderer>().color = tmpColor;
     }
 }
