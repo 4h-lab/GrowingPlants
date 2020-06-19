3a39a00db9349b2101cc62e39ad601b14d26d84c
Gianmarco Biscini <g.biscini@gmail.com>
2020-06-19 18:28:53 +0200

    music still wip

---
+++ Assets/Music and SFX/MusicManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] tracks;
+    private AudioSource audio;
+    private int songIndex;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        songIndex = SceneManager.GetActiveScene().buildIndex % tracks.Length;
+        audio = this.GetComponent<AudioSource>();
+        audio.clip = tracks[songIndex];
+        audio.Play();
+        
+    }
+
+    // Update is called once per frame
+    
+}
