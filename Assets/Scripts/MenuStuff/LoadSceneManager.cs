fdaa61597d062e7eb381100ae727fbfc2462cb94
fedebamba <federicobambagioni@gmail.com>
2020-06-30 09:34:15 -0700

    implemented loading screen

---
+++ Assets/Scripts/MenuStuff/LoadSceneManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public static class LoadSceneManager {
+    private class CoroutineCaller : MonoBehaviour { } // just a dummy class to call the coroutine from
+
+
+    private static CoroutineCaller go= null;
+    private static GameObject loadingScreen = null;
+    private static bool isAlreadyLoading = false;
+
+
+    public static void loadNewLevel(int newLevelIndex) {
+        if (isAlreadyLoading) return;
+
+        if (go == null)  go = new GameObject().AddComponent<CoroutineCaller>();
+        if (loadingScreen == null) loadingScreen = Resources.Load<GameObject>("Screens/loadingScreen");
+        isAlreadyLoading = true;
+
+
+        GameObject xxx = GameObject.Instantiate(loadingScreen, Vector3.zero, Quaternion.identity);
+        xxx.transform.SetParent(GameObject.Find("Canvas").transform);
+
+        go.StartCoroutine(loading(newLevelIndex));
+    }
+
+    public static void loadNewLevel(string newLevelName) {
+        loadNewLevel(SceneManager.GetSceneByName(newLevelName).buildIndex);
+    }
+
+
+    private static IEnumerator loading(int newLevelIndex) {
+        AsyncOperation asyincOp = SceneManager.LoadSceneAsync(newLevelIndex);
+
+        while (!asyincOp.isDone) { 
+
+            float progress = asyincOp.progress;
+            Debug.Log(progress);
+
+            yield return new WaitForEndOfFrame();
+        }
+        isAlreadyLoading = false;
+
+
+    }
+
+}
--- Assets/Scripts/MenuStuff/MainMenuLoader.cs
+++ Assets/Scripts/MenuStuff/MainMenuLoader.cs
@@ -7,7 +7,8 @@ public class MainMenuLoader : MonoBehaviour{
 
     public void loadLevel(string levelname) {
         //todo: use lookup table to get buildindex
-        SceneManager.LoadScene("Level_1");       
+        SceneManager.LoadScene("Level_1");
+        LoadSceneManager.loadNewLevel("Level_1");
     }
 
     public void loadSelectLevel()
