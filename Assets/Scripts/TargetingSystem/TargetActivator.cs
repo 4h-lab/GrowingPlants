3a5f4fc29e5889d4f3bbce36c90999324fc36abb
fedebamba <federicobambagioni@gmail.com>
2020-08-20 09:49:38 -0700

    first commit

---
+++ Assets/Scripts/TargetingSystem/TargetActivator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetActivator : MonoBehaviour{
+    private int numberOfObservers = 0;
+    public GameObject physicalTarget; 
+
+    public void add(TargetManager tm) {
+        numberOfObservers++;
+        if (numberOfObservers == 1) activate(); 
+    }
+    public void remove(TargetManager tm) {
+        numberOfObservers--;
+        if (numberOfObservers == 0) deactivate();
+    }
+
+    private void activate() {
+        //display target.....
+    }
+    private void deactivate() { 
+        //dont display target
+    }
+
+    
+}
---
+++ Assets/Scripts/TargetingSystem/TargetManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetManager: MonoBehaviour{
+    public GameObject physicalTarget;
+    private void OnCollisionEnter2D(Collision collision) {
+        GameObject go = collision.collider.gameObject;
+        if (go.tag == "Player") {
+            if (!go.GetComponent<TargetActivator>()) {
+                go.AddComponent<TargetActivator>();
+                go.GetComponent<TargetActivator>().physicalTarget = physicalTarget;
+            }
+            go.GetComponent<TargetActivator>().add(this);    
+            
+        }
+    }
+    private void OnCollisionExit2D(Collision collision) {
+        collision.collider.gameObject.GetComponent<TargetActivator>().remove(this);
+    }
+
+
+
+}
